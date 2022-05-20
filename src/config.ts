51c0c2c3af9089c4121e68745a2b9f4b40ba31f0
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2022-05-20 19:07:24 +0700

    kk

--- src/config.ts
+++ src/config.ts
@@ -8,10 +8,10 @@ const isDevMode = process.env.NODE_ENV === 'development'
 
 export const config: ISecret = {
   marketContractAddress: isDevMode
-    ? '0xFCCF4eC444c506A766E3cc998f1719F8EBAEAd57'
+    ? '0x72150a723292fF1D2A578961503A48106b51B323'
     : '0x9a697b618CE0B315Bb0D72B3Cf8604ECE0A32F65',
   nftContractAddress: isDevMode
-    ? '0x4a7F81183c405bD5Cb3fc6EAAE8bbE69EB6862B1'
+    ? '0xD53DB759f0826ca01e1Ad3FF2819EBf44141ccC6'
     : '0x7040f31b1C027FF01F038Ee466caADc7DcB5200D',
   dbName: 'kungMarket'
 }
